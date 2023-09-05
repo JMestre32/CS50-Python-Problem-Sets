81c129d4daaf63e4dd943cbcc53085a91b2b6319
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-05 08:52:47 -0700

    Add all existing files to repo

---
+++ Problem-Set-0/playback.py
@@ -0,0 +1,9 @@
+slow = input().split()
+
+# for i in range(len(slow)):
+#     if i < len(slow) cls-1:
+#         print(slow[i] + "...", end="")
+#     else:
+#         print(slow[i])
+
+print("...".join(slow))
