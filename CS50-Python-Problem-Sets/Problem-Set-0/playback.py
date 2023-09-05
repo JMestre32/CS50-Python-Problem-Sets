30d4b8b8818ccc4181e63e716f8f480d034e8f33
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2023-09-05 08:51:57 -0700

    Delete CS50-Python-Problem-Sets directory

--- CS50-Python-Problem-Sets/Problem-Set-0/playback.py
+++
@@ -1,9 +0,0 @@
-slow = input().split()
-
-# for i in range(len(slow)):
-#     if i < len(slow) cls-1:
-#         print(slow[i] + "...", end="")
-#     else:
-#         print(slow[i])
-
-print("...".join(slow))
