926fefa4ca0ebcca4ca64c9a14469c24f809d22d
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-05 19:01:09 -0700

    See body for approach
    Initially, I had a program that worked correctly. However, it failed a test case that tested convert. Although my convert worked fine, it was not the same as the provided code. My convert used 2 parameters, whereas the provided convert function used only one. I had to then figure out how to work with only one parameter and reached this solution. I now pass all the testcases.

--- Problem-Set-1/meal-time.py
+++ Problem-Set-1/meal-time.py
@@ -1,18 +1,20 @@
 def main():
-    time = input("What time is it? ").strip().split(":")
-    meal = convert(time[0], time[1])
-    if meal > 7 and meal < 8:
+    time = input("What time is it? ").strip()
+    meal = convert(time)
+    if meal >= 7 and meal <= 8:
         print("breakfast time")
-    elif meal > 12 and meal < 13:
+    elif meal >= 12 and meal <= 13:
         print("lunch time")
-    elif meal > 18 and meal < 19:
+    elif meal >= 18 and meal <= 19:
         print("dinner time")
     else:
         exit()
 
 
-def convert(hour, minute):
-    return(float(hour) + float(minute)/60)
+def convert(time):
+    new_time = (time.split(":"))
+    return(float(new_time[0]) + float(new_time[1])/60)
 
 
-main()
+if __name__ == "__main__":
+    main()
