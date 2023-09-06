f807fa16e745c93f159c411d45567033e9336d87
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-05 18:14:44 -0700

    Fix float conversion from y to z

--- Problem-Set-1/math-interpreter.py
+++ Problem-Set-1/math-interpreter.py
@@ -1,14 +1,14 @@
-expr = input("Expression: ").strip().split()
+expr = (input("Expression: ").strip().split())
 
 x = expr[0]
 y = expr[1]
 z = expr[2]
 
 if y == '+':
-    print(float(x) + float(y))
+    print(float(x) + float(z))
 elif y == '-':
-    print(float(x) - float(y))
+    print(float(x) - float(z))
 elif y == '*':
-    print(float(x) * float(y))
+    print(float(x) * float(z))
 elif y == '/':
-    print(float(x) / float(y))    
+    print(float(x) / float(z))    
