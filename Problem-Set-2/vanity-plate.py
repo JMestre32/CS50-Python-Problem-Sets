e9ff1df2d22dd663a6f2bb3045116862f8b208a9
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-07 09:24:57 -0700

    Revise soln. tidy up :

--- Problem-Set-2/vanity-plate.py
+++ Problem-Set-2/vanity-plate.py
@@ -7,53 +7,48 @@ def main():
 
 
 def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-
+    #Ensure the first two characters on the plate are letters
     if s[0].isalpha() == False or s[1].isalpha() == False:
         return False
+    #Ensure the plate's least 2 characters long and 6 characters max
+    if len(s) < 2 or len(s) > 6:
+        return False
     
+    #Return false if a digit is in the middle of the plate
     i = 0
-    if s[0:].isalpha():
-        return True
-    
     while i < len(s):
-        if s[i].isalpha() == False:
+        if s[i].isdigit() == True and s[i:].isdigit() == False:
+            return False
+        i += 1
+    
+    #Return false if the first digit on the plate is '0'
+    i = 0
+
+    #Iterate through s
+    #If first instance of s[i] where s[i].isdigit is true
+    #is == '0' return False
+    #Otherwise, break out
+    while i < len(s):
+        print(s[i])
+        if s[i].isdigit() == True:
             if s[i] == '0':
                 return False
             else:
                 break
-        i += 1
-
-    for letter in s:
-        if letter in [' ', '.', '!', '?']:
-            return False
+        i +=1
+        
         
 
-    for letter in range(len(s)):
-        if s[0].isalpha() and s[1].isalpha():
-            if len(s) == 2:
-                return True
-            elif len(s) ==3:
-                return True
-            elif len(s) == 4:
-                if s[2].isdigit() and s[3].isdigit():
-                    return True
-            elif len(s) == 5:
-                if s[2].isdigit() and s[3].isdigit() and s[4].isdigit():
-                    return True
-                elif s[3].isdigit() and s[4].isdigit():
-                    return True
-            elif len(s) ==6:
-                if s[2].isdigit() and s[3].isdigit() and s[4].isdigit() and s[5].isdigit():
-                    return True
-                elif s[3].isdigit() and s[4].isdigit() and s[5].isdigit():
-                    return True
-                elif s[4].isdigit() and s[5].isdigit():
-                    return True
+    #Return false if there are punctuation marks in the plate
+    for letter in s:
+        if letter in [',', '.', '!', ' ', '?']:
+            return False
+        
+    return True
 
 
 
+main()
 
 #Hint:
 #Much like a list, a str is a “sequence” (of characters),
@@ -61,13 +56,3 @@ def is_valid(s):
 # syntax like s[i:j]. For instance, if s is "CS50", then
 #s[0:2] would be "CS".
 
-main()
-
-
-
-
-    # while i <= len(s):
-    #     if s[i].isalpha() == False:
-    #         if s[i] == '0':
-    #             return False
-    #     i += 1
