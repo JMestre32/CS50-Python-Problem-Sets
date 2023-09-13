8acee3bcd278a98c2ec726d0ad3dcdf36234fe53
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-13 12:59:12 -0700

    Finished. Testing now.

--- Problem-Set-3/grocery.py
+++ Problem-Set-3/grocery.py
@@ -1,26 +1,28 @@
-#Initialize an empty dictionary
-grocery_list = {}
-
-#Prompt the user for input with an empty terminal
-
-
-
-
-#Use an infinite while loop
-i = 0
-while True:
+#Initialize an empty dict
+groceries = {}
+while True: 
     try:
-        list_item = input().upper()
-        for key in grocery_list:
-            if list_item in grocery_list:
-                key["num"] = i + 1
+        item = input().upper()
+        #Check to see if the item exists, if it does update it's item count
+        if item in groceries:
+            groceries.update({item: groceries[item] + 1})
+        #Otherwise, add the item and initialize it's count to 1
         else:
-            print("grocery_list dict: " + str(grocery_list))
-            grocery_list["name"] = list_item
-            grocery_list["num"] = i+1
+            groceries.update({item: 1})
     except EOFError:
-        for key in grocery_list:
-            print(grocery_list["name"] + " " + str(grocery_list["num"]))
-            exit()
+        #Take the keys in groceries and turn that into a list
+        sorted = list(groceries.keys())
+        #Sort the list of keys by alphabetical order using the list method sort()
+        sorted.sort()
+
+        #So, for each item, in the sorted list (line 16), the line below creates a key-value 
+        #pair in the new sorted_dict whose keys are each item in the sorted list and whose 
+        #values are the items corresponding counts from the groceries dict. 
+        sorted_dict = {item: groceries[item] for item in sorted}
+
+        #Print the sorted dict value first, then key
+        for i in sorted_dict:
+            print(sorted_dict[i], i)
+        exit()
     except KeyError:
-        print("key error")
+        pass
