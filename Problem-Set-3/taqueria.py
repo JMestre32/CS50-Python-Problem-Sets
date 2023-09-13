1fce05edb0885df353943d598ed1635e6a3d6904
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-13 09:19:02 -0700

    Shell of assignment

---
+++ Problem-Set-3/taqueria.py
@@ -0,0 +1,24 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+total = 0
+while True: 
+    #Get order, if it's not in menu, reprompt
+    #If it's in the menu, print the total value 
+    #(like a total = item + total)
+    #figure out which errors to catch and catch 'em
+    try: 
+        food = input("Item: ")
+    except EOFError:
+        exit()
+
+    else:
+        print("Total: " + total)
