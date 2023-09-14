621a111cbceb197a56c19eeb2379c3a586aec44e
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-14 11:37:52 -0700

    See body for details
    Initial program passed half the tests, just had to quit program after printing.
    Additionally, the first if statement was printing incorrectly (padded 0's issue again).
    By turning the manipulated data into an in like in the second if-statement the problem
    was resolved. All test cases passed. EZ.

--- Problem-Set-3/outdated.py
+++ Problem-Set-3/outdated.py
@@ -19,25 +19,28 @@ while i <=12:
     months_dict.update({months[i-1]: i})
     i +=1
 
-while True: 
-    try: 
+while True:
+    try:
         #Get the user's input
         date = input("Date: ")
 
         #Use get method, if it returns an error then we have the wrong info
         for i in months_dict:
-            if date.split()[0] in i:    
-                month = months_dict.get(date.split()[0])              
+            if date.split()[0] in i:
+                month = int(months_dict.get(date.split()[0]))
+                if ',' not in date:
+                    continue
                 first_half = (date.split(','))
-                day = first_half[0].split()[1]
+                day = int(first_half[0].split()[1])
                 year = date.split()[2]
-                
+
                 if month is None:
                     continue
                 elif int(day) > 31:
                     continue
                 else:
-                    print(year + "-" + f'{month:02}' + "-" + f'{day:02}')
+                    print(year + "-" + f'{month:02}' + "-" + f'{day:02}', end="")
+                    quit()
 
 
         if 1 <= int(date.split('/')[0]) <= 12 and 1 <= int(date.split('/')[1]) <= 31:
@@ -45,6 +48,7 @@ while True:
             day = int(date.split('/')[1])
             year = int(date.split('/')[2])
             print(str(year) + "-" + f'{month:02}' + "-" + f'{day:02}')
+            quit()
 
     except EOFError:
         exit()
